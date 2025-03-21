a6f3fefb9e3b2eed7ca0295b1c67470f34f1d62b
dabrorius <filip.defar@protonmail.com>
2025-03-21 21:12:18 +0100

    Extract reading of ROM to own function

--- src/main.c
+++ src/main.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+  unsigned char code[4];
+} Chip8Instruction;
+
+void readRomFile(const char *fileName, Chip8Instruction **program, int *programSize);
+
 int main()
 {
   printf("Welcome to Chippy!\n");
 
-  FILE *file = fopen("./roms/1-chip8-logo.ch8", "rb");
+  Chip8Instruction *program = NULL;
+  int programSize = 0;
+
+  readRomFile("./roms/1-chip8-logo.ch8", &program, &programSize);
+
+  for (int i = 0; i < programSize; i++)
+  {
+
+    for (int j = 0; j < 2; j++)
+    {
+      printf("%02x", program[i].code[j]);
+    }
+
+    printf("\n");
+  }
+
+  return 0;
+}
+
+void readRomFile(const char *fileName, Chip8Instruction **program, int *programSize)
+{
+  FILE *file = fopen(fileName, "rb");
 
   if (file == NULL)
   {
@@ -19,14 +47,18 @@ int main()
 
   while ((bytesRead = fread(instruction, 1, sizeof(instruction), file)))
   {
+    *programSize = *programSize + 1;
+
+    *program = realloc(*program, *programSize * sizeof(Chip8Instruction));
+
     for (int i = 0; i < bytesRead; i++)
     {
-      printf("%02x", instruction[i]);
+      // printf("%02x", instruction[i]);
+      (*program)[*programSize - 1].code[i] = instruction[i];
     }
 
-    printf("\n");
+    // printf("\n");
   }
 
   fclose(file);
-  return 0;
 }
