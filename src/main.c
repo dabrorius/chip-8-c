8b3ff65c079090e7eb89f2e295135849fd14db85
dabrorius <filip.defar@protonmail.com>
2025-03-21 18:57:40 +0100

    Basic reading of ROM

--- src/main.c
+++ src/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-  printf("Hello world!");
+  printf("Welcome to Chippy!\n");
 
+  FILE *file = fopen("./roms/1-chip8-logo.ch8", "rb");
+
+  if (file == NULL)
+  {
+    fputs("File error", stderr);
+    exit(1);
+  }
+
+  const int instructionSize = 2;
+  unsigned char instruction[instructionSize];
+  size_t bytesRead;
+
+  while ((bytesRead = fread(instruction, 1, sizeof(instruction), file)))
+  {
+    for (int i = 0; i < bytesRead; i++)
+    {
+      printf("%02x", instruction[i]);
+    }
+
+    printf("\n");
+  }
+
+  fclose(file);
   return 0;
 }
