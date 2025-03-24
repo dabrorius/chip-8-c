43f39a114ffbf70f16fc6475e1fad4a8868f857f
dabrorius <filip.defar@protonmail.com>
2025-03-24 19:46:56 +0100

    Basic screen implementation

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 #include "load_rom.h"
+
+#define SCREEN_WIDTH 64
+#define SCREEN_HEIGHT 32
+#define PIXEL_SIZE 16
 
 int main()
 {
@@ -26,9 +31,14 @@ int main()
 
   free(program);
 
+  bool screen[SCREEN_WIDTH][SCREEN_HEIGHT] = {false};
+
+  screen[1][1] = true;
+  screen[2][1] = true;
+
   SDL_Init(SDL_INIT_VIDEO);
 
-  SDL_Window *window = SDL_CreateWindow("Chippy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, 0);
+  SDL_Window *window = SDL_CreateWindow("Chippy", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH * PIXEL_SIZE, SCREEN_HEIGHT * PIXEL_SIZE, 0);
 
   SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
@@ -49,8 +59,18 @@ int main()
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_Rect square = {100, 100, 50, 50};
-    SDL_RenderFillRect(renderer, &square);
+
+    for (int column = 0; column < SCREEN_WIDTH; column++)
+    {
+      for (int row = 0; row < SCREEN_HEIGHT; row++)
+      {
+        if (screen[column][row])
+        {
+          SDL_Rect square = {column * PIXEL_SIZE, row * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE};
+          SDL_RenderFillRect(renderer, &square);
+        }
+      }
+    }
 
     SDL_RenderPresent(renderer);
     SDL_Delay(16);
